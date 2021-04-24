a6721adef37936c948a9dfeec68585180c04b71d
sandy <sandy.rav@onja.org>
2021-04-24 14:43:28 +0300

    made mofication on the image types

--- src/components/ActionButtonsComponent/ActionButtons.js
+++ src/components/ActionButtonsComponent/ActionButtons.js
@@ -1,27 +1,61 @@
 // components/Task.js
 import * as React from 'react';
-import {Image, View, TouchableOpacity, Text } from 'react-native';
+import {Image, View,SelfAreaView, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import Learn from '../../assets/learn_icon.svg'
+// import Correct from '../../assets/correct_icon.svg';
+// import Wrong from '../../assets/wrong_icon.svg';
+
+const styles = StyleSheet.create({
+  container: {
+    textAlign: 'center',
+    padding: 23,
+    fontSize: 16,
+    lineHeight: 19,
+    fontWeight: '600',
+  },
+  button: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+
+  defaultText: {
+    color: '#06B6D4',
+  },
+
+  correctText: {
+    color: '#06D440',
+  },
+
+  wrongText: {
+    color: '#D4068E',
+  },
+
+});
 
 
 export default function ActionButtons({buttonText, onPressFunction}) {
 
+console.log(Learn)
+
   return (
-      <View>
+      <SelfAreaView style={styles.container}>
         <TouchableOpacity 
           onPress={onPressFunction}
+          style={styles.button}
         >
-        <Text>{buttonText}</Text>
-        {
-          buttonText === 'Pick' ?
-          <Image source={require('../assets/pick.png')} />
-          : buttonText === 'Correct' ?
-          <Image source={require('../assets/correct.png')} />
-          : buttonText === 'Wrong' ?
-          <Image source={require('../assets/wrong.png')} />
-          :
-          ''
-        }
+        <Text 
+          style={
+            buttonText === 'Pick' ?
+            styles.defaultText :
+            buttonText === 'Correct' ?
+            styles.correctText :
+            styles.wrongText
+          }
+        >
+          {buttonText}
+        </Text>
+         <Learn />
         </TouchableOpacity>
-      </View>
+      </SelfAreaView>
   );
 }
