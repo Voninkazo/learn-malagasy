2acb9be0506eb43eec335f998e608ee7e57c4df4
sandy <sandy.rav@onja.org>
2021-04-27 14:44:39 +0300

    added styles

--- src/components/LanguageSwitcherComponent/LanguageSwitcher.js
+++ src/components/LanguageSwitcherComponent/LanguageSwitcher.js
@@ -3,6 +3,32 @@ import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import ArrowSwitcher from './assets/arrow.svg';
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 23,
+  },
+  button: {
+    width: 90,
+    height: 40,
+    borderRadius: 30,
+    backgroundColor: '#06B6D4',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 13,
+    lineHeight: 16,
+    fontWeight: '400',
+    color: '#ffffff',
+    textTransform: 'uppercase',
+  },
+  icon: {
+    marginRight: 6,
+    marginLeft: 6,
+  },
+});
+
 export default function LanguageSwitcher({
   malagasySwitcher,
   englishSwitcher,
@@ -10,11 +36,15 @@ export default function LanguageSwitcher({
   isEnglish,
 }) {
   return (
-    <View>
-      <TouchableOpacity onPress={onPressFunction}>
-        <Text>{isEnglish ? englishSwitcher : malagasySwitcher}</Text>
-        <ArrowSwitcher />
-        <Text>{isEnglish ? malagasySwitcher : englishSwitcher}</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPressFunction} style={styles.button}>
+        <Text style={styles.text}>
+          {isEnglish ? englishSwitcher : malagasySwitcher}
+        </Text>
+        <ArrowSwitcher style={styles.icon} />
+        <Text style={styles.text}>
+          {isEnglish ? malagasySwitcher : englishSwitcher}
+        </Text>
       </TouchableOpacity>
     </View>
   );
