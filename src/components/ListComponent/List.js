bec7458fe5aa6e52d19e9037f32756f132b0a223
sandy <sandy.rav@onja.org>
2021-04-28 14:40:53 +0300

    used more props to make the component reusable and wrapped each list inside a touchableopacity to make the whole list item clickable

--- src/components/ListComponent/List.js
+++ src/components/ListComponent/List.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import {ScrollView, View, StyleSheet} from 'react-native';
+import {ScrollView, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 import SectionHeading from '../SectionHeadingComponent/SectionHeading';
 import ListItem from '../ListItemComponent/ListItem';
-import Categories from '../../data/categories.json';
 
 const styles = StyleSheet.create({
   container: {
@@ -15,20 +14,24 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function List() {
-  let categories = Categories['categories'];
-
+export default function List({
+  heading,
+  isEnglish,
+  listsToDisplay,
+  onPressFunction,
+}) {
   return (
     <ScrollView style={styles.container}>
-      <SectionHeading text="Select a category:" />
+      <SectionHeading text={heading} />
       <View style={styles.listContainer}>
-        {categories.map(cat => {
+        {listsToDisplay.map(cat => {
           return (
-            <ListItem
-              key={cat.id}
-              item={cat.name['en']}
-              onPressFunction={() => alert('learn')}
-            />
+            <TouchableOpacity onPress={onPressFunction} key={cat.id}>
+              <ListItem
+                item={isEnglish ? cat.name['en'] : cat.name['mg']}
+                onPressFunction={onPressFunction}
+              />
+            </TouchableOpacity>
           );
         })}
       </View>
